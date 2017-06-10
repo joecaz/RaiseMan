6ee503aee8200c12eeaec0b74647ee2e87a0fd80
Joseph Cicinelli <jcicinelli@gmail.com>
2017-06-10 09:57:42 -0700

    Initial Commit

---
+++ RaiseMan/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RaiseMan
+//
+//  Created by joecaz on 6/10/17.
+//  Copyright © 2017 Coyote Creek Software. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
