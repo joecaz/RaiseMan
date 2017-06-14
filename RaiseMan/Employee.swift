f1a02a3de8f97b94ba5d0b17c8fb21833d307722
Joseph Cicinelli <jcicinelli@gmail.com>
2017-06-14 15:10:21 -0700

    Chapter 10 implemented, formatters Chapter, compiles in Swift 3.1

--- RaiseMan/Employee.swift
+++ RaiseMan/Employee.swift
@@ -11,4 +11,16 @@ import Foundation
 class Employee: NSObject {
     var name: String? = "New Employee"
     var raise: Float = 0.05
+    
+    override func validateValue(_ ioValue: AutoreleasingUnsafeMutablePointer<AnyObject?>, forKey inKey: String) throws {
+        print ("Entered validation function")
+        
+        let raiseNumber = ioValue.pointee // "memory" is now "pointee" in Swift 3
+        if inKey == "raise" && raiseNumber == nil {
+            let domain = "UserInputValidationErrorDomain"
+            let code = 0
+            let userInfo = [NSLocalizedDescriptionKey : "An employee's raise must be a number."]
+            throw NSError(domain: domain, code: code, userInfo: userInfo)
+        }
+    }
 }
